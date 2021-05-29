afa8ca2eb36e4f66796ecc51cfafdd37af2867d0
[SotaMatsuzawa] <[gglsouta2143@gmail.com]>
2021-05-29 19:14:00 +0900

    add Logit model example

--- Day2_mlflow/train.py
+++ Day2_mlflow/train.py
@@ -12,7 +12,8 @@ def objective(trial):
     iris = datasets.load_iris()
     X = iris.data
     y = iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
 
     train_set = lgb.Dataset(X_train, label=y_train)
     test_set = lgb.Dataset(X_test, label=y_test)
@@ -21,14 +22,15 @@ def objective(trial):
                  lambda_l1=trial.suggest_loguniform("lambda_l1", 1e-8, 10.0),
                  lambda_l2=trial.suggest_loguniform("lambda_l2", 1e-8, 10.0),
                  num_leaves=trial.suggest_int("num_leaves", 2, 256),
-                 feature_fraction=trial.suggest_uniform("feature_fraction", 0.4, 1.0),
-                 bagging_fraction=trial.suggest_uniform("bagging_fraction", 0.4, 1.0),
+                 feature_fraction=trial.suggest_uniform(
+                     "feature_fraction", 0.4, 1.0),
+                 bagging_fraction=trial.suggest_uniform(
+                     "bagging_fraction", 0.4, 1.0),
                  bagging_freq=trial.suggest_int("bagging_freq", 1, 7),
                  min_child_samples=trial.suggest_int("min_child_samples", 5, 100), seed=42)
 
     gbm = lgb.train(param, train_set, valid_sets=test_set)
     y_proba = gbm.predict(X_test)
-    y_pred = np.argmax(y_proba, axis=1)
     loss = log_loss(y_test, y_proba)
 
     return loss
@@ -40,7 +42,7 @@ def main():
     mlflow.lightgbm.autolog()
 
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials=10)
 
     print("Number of finished trials: {}".format(len(study.trials)))
 
